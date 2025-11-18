26c161f0da3384f6a85a2808c1fe617c678b61cb
salomonmmunyaneza <munyanezajoel3@gmail.com>
2025-11-18 13:02:31 +0200

    python3

---
+++ basic_operations.py
@@ -0,0 +1,13 @@
+# Predefined numbers
+number1 = 10
+number2 = 5
+
+# Arithmetic operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# Output results
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
